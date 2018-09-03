ebeb679dac2e44a1d4883560f7d75ba64fcf7e7b
rynxiao <yuzhongzi91@sina.com>
2018-09-03 22:15:00 +0800

    feat: add login & qrcode

---
+++ src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="login">
+    <div class="login__qrcode">
+      <qrcode value="Hello, World!" :options="{ size: 200 }"></qrcode>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueQrcode from '@xkeshi/vue-qrcode'
+
+Vue.component(VueQrcode.name, VueQrcode)
+
+export default {
+  name: 'login'
+}
+</script>
+
+<style scoped>
+.login__qrcode {
+  margin-top: 100px;
+  width: 100%;
+  height: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
